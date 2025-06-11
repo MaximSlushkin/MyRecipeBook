8781b1a44b0d2dd013bef3e6a0abb268e62d62b2
MakcSlushkin <slushkin.makcum@yandex.ru>
2025-06-11 12:44:38 +0300

    RA-04: 1. Имплементировал библиотеку fragment-ktx в build.gradle.kts 2. Создал CategoriesListFragment.kt и лейаут для него fragment_list_categories.xml: -Унаследоваk от Fragment -Проинициализировал переменную binding в методе onCreateView() 3. В fragment_list_categories.xml задал стандартный фон по макету и разместил TextView с названием фрагмента 4. В activity_main.xml создал контейнер для фрагментов FragmentContainerView с id mainContainer. 5. Добавил поддержку edge-to-edge

--- app/src/main/java/com/example/myrecipebook/MainActivity.kt
+++ app/src/main/java/com/example/myrecipebook/MainActivity.kt
@@ -1,17 +1,34 @@
 package com.example.myrecipebook
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import com.example.myrecipebook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        enableEdgeToEdge()
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
+            val systemBarsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(
+                top = systemBarsInsets.top,
+                bottom = systemBarsInsets.bottom
+            )
+            insets
+        }
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainContainer, CategoriesListFragment())
+            .commit()
     }
 }
