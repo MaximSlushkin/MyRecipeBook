47d5137d45879b5a38489cde6669d9cc731e0e4e
MakcSlushkin <slushkin.makcum@yandex.ru>
2025-06-15 11:26:21 +0300

    RA-05 Changes: 1. Исправлено дублирование class MainActivity 2. Использование  replace<FavoritesFragment>(R.id.mainContainer) из библиотеки ktx вместо стандартной replace(R.id.mainContainer, CategoriesListFragment()) 3. Следование установленному дизайну в соответствии с макетом цвету кнопок 4. Цвет текста "Категории", "Избранное" и цвет иконки ic_heart_empty установлен на белый т.к. при запуске на смарфтоне они синие

--- app/src/main/java/com/example/myrecipebook/MainActivity.kt
+++ app/src/main/java/com/example/myrecipebook/MainActivity.kt
@@ -1,83 +1,66 @@
 package com.example.myrecipebook
 
-import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.example.myrecipebook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    class MainActivity : AppCompatActivity() {
-        private var _binding: ActivityMainBinding? = null
-        private val binding: ActivityMainBinding
-            get() = _binding
-                ?: throw IllegalStateException("Binding for ActivityMainBinding must not be null")
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+        get() = _binding
+            ?: throw IllegalStateException("Binding for ActivityMainBinding must not be null")
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            enableEdgeToEdge()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
 
-            super.onCreate(savedInstanceState)
-            _binding = ActivityMainBinding.inflate(layoutInflater)
-            setContentView(binding.root)
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-            ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
-                val systemBarsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-                view.updatePadding(
-                    top = systemBarsInsets.top,
-                    bottom = systemBarsInsets.bottom
-                )
-                insets
-            }
-            if (savedInstanceState == null) {
-                navigateToCategories()
-            }
-
-            binding.btnRecipes.setOnClickListener {
-                navigateToCategories()
-            }
-
-            binding.btnFavorites.setOnClickListener {
-                navigateToFavorites()
-            }
-        }
-
-        private fun navigateToCategories() {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                replace(R.id.mainContainer, CategoriesListFragment())
-            }
-            updateButtonColors(true)
-        }
-
-        private fun navigateToFavorites() {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                replace(R.id.mainContainer, FavoritesFragment())
-            }
-            updateButtonColors(false)
-        }
-
-        private fun updateButtonColors(isCategoriesActive: Boolean) {
-            val activeColor = ContextCompat.getColor(this, R.color.button_active_color)
-            val inactiveColor = ContextCompat.getColor(this, R.color.button_inactive_color)
-
-            binding.btnRecipes.backgroundTintList = ColorStateList.valueOf(
-                if (isCategoriesActive) activeColor else inactiveColor
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
+            val systemBarsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(
+                top = systemBarsInsets.top,
+                bottom = systemBarsInsets.bottom
             )
-            binding.btnFavorites.backgroundTintList = ColorStateList.valueOf(
-                if (isCategoriesActive) inactiveColor else activeColor
-            )
+            insets
+        }
+        if (savedInstanceState == null) {
+            navigateToCategories()
         }
 
-        override fun onDestroy() {
-            super.onDestroy()
-            _binding = null
+        binding.btnRecipes.setOnClickListener {
+            navigateToCategories()
         }
+
+        binding.btnFavorites.setOnClickListener {
+            navigateToFavorites()
+        }
+    }
+
+    private fun navigateToCategories() {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace<CategoriesListFragment>(R.id.mainContainer)
+        }
+    }
+
+    private fun navigateToFavorites() {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace<FavoritesFragment>(R.id.mainContainer)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
