263975562f04178793a133f50e70ce3397d9b6bb
MakcSlushkin <slushkin.makcum@yandex.ru>
2025-06-16 11:29:43 +0300

    RA-06 Changes: 1. Создал для каждой из сущностей Category, Recipe, Ingredient отдельный класс. 2. Изменил свойства классов как в модели JSON.

---
+++ app/src/main/java/com/example/myrecipebook/Ingredient.kt
@@ -0,0 +1,7 @@
+package com.example.myrecipebook
+
+data class Ingredient(
+    val quantity: String,
+    val unitOfMeasure: String,
+    val description: String,
+)
--- app/src/main/java/com/example/myrecipebook/recipes.kt
+++ app/src/main/java/com/example/myrecipebook/recipes.kt
@@ -4,39 +4,40 @@ object STUB {
 
     private val categories = listOf(
         Category(
-            id = 1,
-            name = "Салаты",
-            url = "salad.png"
+            id = 0,
+            title = "Бургеры",
+            description = "Рецепты всех популярных видов бургеров",
+            imageUrl = "burger.png"
         ),
-
+        Category(
+            id = 1,
+            title = "Десерты",
+            description = "Самые вкусные рецепты десертов специально для вас",
+            imageUrl = "dessert.png"
+        ),
         Category(
             id = 2,
-            name = "Супы",
-            url = "soups.png"
+            title = "Пицца",
+            description = "Пицца на любой вкус и цвет. Лучшая подборка для тебя",
+            imageUrl = "pizza.png"
         ),
-
         Category(
             id = 3,
-            name = "Десерты",
-            url = "desserts.png"
+            title = "Рыба",
+            description = "Печеная, жареная, сушеная, любая рыба на твой вкус",
+            imageUrl = "fish.png"
         ),
-
         Category(
             id = 4,
-            name = "Бургеры",
-            url = "burger.png"
+            title = "Супы",
+            description = "От классики до экзотики: мир в одной тарелке",
+            imageUrl = "soup.png"
         ),
-
         Category(
             id = 5,
-            name = "Рыба",
-            url = "fish.png"
-        ),
-
-        Category(
-            id = 6,
-            name = "Пицца",
-            url = "pizza.png"
+            title = "Салаты",
+            description = "Хрустящий калейдоскоп под соусом вдохновения",
+            imageUrl = "salad.png"
         )
     )
 
